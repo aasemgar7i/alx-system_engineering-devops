79c764c387faefb1843f0502972b54c24bfba797
aasemgar7i <aasemgar7i@gamil.com>
2024-05-10 00:48:18 +0300

    noun

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,8 @@ def number_of_subscribers(subreddit):
         number of subscribers to the subreddit,
         or 0 if subreddit requested is invalid"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Reddit API test"}
-    response = requests.get(url, headers=headers)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
